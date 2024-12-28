cd5c88027b6fac503524a80aeab8b9ade297d13f
Bharath Suntramoni <bharathsuntramoni@gmail.com>
2024-12-28 22:39:07 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,10 +2,10 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
+  name: "Bharath",
   initials: "DV",
   url: "https://dillion.io",
-  location: "San Francisco, CA",
+  location: "Hyderabad",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
     "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
